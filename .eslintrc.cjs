94c669a00d8a3a1f18d625ac95aee94715cf9f40
Kthommd <kgtrocks@gmail.com>
2025-08-14 16:15:00 -0700

    chore: add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: false,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'react-hooks'],
+  rules: {
+    // Customize or override rules here. For example, you can require
+    // explicit return types on functions or disable prop-types if you use TypeScript.
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
