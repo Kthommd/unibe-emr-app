7d9cd278736aab75522831e8886ebf0b44b56076
Kthommd <kgtrocks@gmail.com>
2025-08-14 17:23:30 -0700

    chore: add husky init script

---
+++ _husky.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Husky shell script to prepare environment for hooks
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
+  }
+
+  readonly hook_name="$(basename "$0")"
+  debug "$hook_name hook started..."
+
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook"
+    exit 0
+  fi
+
+  if [ -f ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+
+  export readonly husky_skip_init=1
+  sh -e "$0" "$@"
+  exitCode="$?"
+  debug "$hook_name hook finished with exit code $exitCode"
+  exit $exitCode
+fi
